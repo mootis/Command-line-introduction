6137548c08c83c812a7773363db789665202bf49
HuntsmanRoym <grins008@morris.umn.edu>
2018-09-04 09:54:39 -0500

    Completed part 1, tests pass for compiling

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-number = $1
-directory = $2
+
+# Extract NthPrime.tgz > Specified directory - Uncompress then extract
+# Blake guided our thinking here
+ tar -xzf NthPrime.tgz -C $2
+
+# Compile files to generate the binary
+
+# Sets current directory to directory above that's passed in executable
+ cd $2/NthPrime
+ gcc -o NthPrime main.c nth_prime.h nth_prime.c 
+# exec ./extract_and_compile.sh $1 $2 - This is an ex. of something NOT to do
+ ./NthPrime $1
+
